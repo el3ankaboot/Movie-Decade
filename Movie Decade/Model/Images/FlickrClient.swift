e70b2d90af2faad2d5049db664cbc26a051a17d1
Gamal Gamaleldin <gamal.m.esmail@gmail.com>
2019-10-13 19:45:25 +0200

    Cache the images URLs.

--- Movie Decade/Model/Images/FlickrClient.swift
+++ Movie Decade/Model/Images/FlickrClient.swift
@@ -14,6 +14,7 @@ class FlickrClient {
     static let apiKey = "3e7eeb4c8932f66add6ddb8a08ca63aa"
     static let secret = "798bb5c5a6b7e6da"
     static let imageCache = NSCache<NSString, UIImage>()
+    static let imageUrlsCache = NSCache<NSString,NSArray>()
     
     enum Endpoints {
         static let baseURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(apiKey)"
@@ -34,47 +35,52 @@ class FlickrClient {
     }//Closing of enum
     
     class func getImagesURLs (movieTitle:String, completion: @escaping ([ImageUrl]? , String) -> Void){
-        print(Endpoints.search(movieTitle).url)
-        Alamofire.request(Endpoints.search(movieTitle).url, method: .get, encoding: JSONEncoding.default, headers: [:])
-            .responseJSON { response in
-                switch response.result {
-                case .success:
-                    print("Success")
-                case .failure(_):
-                    completion(nil,"Failed to retrieve images.")
-                    print("Failure")
-                }
-            }
-            .response { response in
-                if let data = response.data {
-                    switch response.response?.statusCode {
-                    case 200 :
-                        let json = JSON(data)
-                        let photos = json["photos"]
-                        let photo = photos["photo"]
-                        var imageURLs = [ImageUrl]()
-                        var returnCount = 0
-                        for p in photo {
-                            let farmID = p.1["farm"].int ?? 0
-                            let serverID = p.1["server"].string ?? "1"
-                            let id = p.1["id"].string ?? "1"
-                            let secret = p.1["secret"].string ?? ""
-                            let imageUrl = ImageUrl(farmID: farmID, serverID: serverID, id: id, secret: secret)
-                            imageURLs.append(imageUrl)
-                            returnCount += 1
-                            if returnCount == photo.count {completion(imageURLs ,"")}
-                        }
-                        if photo.count == 0{
-                            completion([],"Empty")
-                        }
-                        
-                        
-                    default :
-                        completion(nil ,"Failed to retrieve images.")
+        if let imagesInCache = imageUrlsCache.object(forKey: movieTitle as NSString) as? [ImageUrl] {
+            completion(imagesInCache,"")
+        }else {
+            Alamofire.request(Endpoints.search(movieTitle).url, method: .get, encoding: JSONEncoding.default, headers: [:])
+                .responseJSON { response in
+                    switch response.result {
+                    case .success:
+                        print("Success")
+                    case .failure(_):
+                        completion(nil,"Failed to retrieve images.")
+                        print("Failure")
                     }
-                    
                 }
-        }
+                .response { response in
+                    if let data = response.data {
+                        switch response.response?.statusCode {
+                        case 200 :
+                            let json = JSON(data)
+                            let photos = json["photos"]
+                            let photo = photos["photo"]
+                            var imageURLs = [ImageUrl]()
+                            var returnCount = 0
+                            for p in photo {
+                                let farmID = p.1["farm"].int ?? 0
+                                let serverID = p.1["server"].string ?? "1"
+                                let id = p.1["id"].string ?? "1"
+                                let secret = p.1["secret"].string ?? ""
+                                let imageUrl = ImageUrl(farmID: farmID, serverID: serverID, id: id, secret: secret)
+                                imageURLs.append(imageUrl)
+                                returnCount += 1
+                                if returnCount == photo.count {completion(imageURLs ,"")}
+                                imageUrlsCache.setObject(imageURLs as NSArray, forKey: movieTitle as NSString)
+                            }
+                            if photo.count == 0{
+                                completion([],"Empty")
+                                imageUrlsCache.setObject([] as NSArray, forKey: movieTitle as NSString)
+                            }
+                            
+                            
+                        default :
+                            completion(nil ,"Failed to retrieve images.")
+                        }
+                        
+                    }
+            }
+        }//Closing of else (Not cached)
     }//Closing of getImagesURLs Func
     
     class func images(imageUrlString: ImageUrl?, cell: ImagesCell ) {
